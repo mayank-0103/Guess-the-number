6db1b3d3448f3582da62be6a7fe16121e680b498
mayank-0103 <201279422+mayank-0103@users.noreply.github.com>
2025-03-13 22:47:55 +0530

    Added nav bar

--- script.js
+++ script.js
@@ -127,3 +127,49 @@ animateText();
 
 // this is added when use resize window, animation follows it
 window.onresize = animateText;
+
+// Capturing header elements
+const about_game = document.querySelector('.headers>#game');
+const author = document.querySelector('.headers>#author');
+const game_detail = document.querySelector('.about-game');
+const author_detail = document.querySelector('.author-details');
+
+about_game.addEventListener("click", function() {
+    game_detail.classList.add("show");
+    document.body.querySelector('.black').classList.add("black-bg");
+});
+
+author.addEventListener("click", function() {
+    author_detail.classList.add("show");
+    document.body.querySelector('.black').classList.add("black-bg");
+});
+
+// Adding close Functionality
+let button_1 = document.querySelector('.about-game > .close');
+let button_2 = document.querySelector('.author-details > .close');
+
+function closeWithAnimation(element) {
+    element.classList.remove("show");
+    element.classList.add("hide");
+    setTimeout(() => {
+        element.classList.remove("hide");
+        document.body.querySelector('.black').classList.remove("black-bg");
+    }, 500); // Matches the CSS animation duration
+}
+
+button_1.addEventListener("click", function () {
+    closeWithAnimation(game_detail);
+});
+
+button_2.addEventListener("click", function () {
+    closeWithAnimation(author_detail);
+});
+
+// Adding Home Link
+const home = document.querySelector('.headers>#home');
+home.addEventListener("click", function () {
+    let userResponse = confirm("You are being redirected to an external site\nhttps://mayank-0103.github.io/mayank-0103/\nDo you want to continue?");
+    if (userResponse) {
+        window.location.href = "https://mayank-0103.github.io/mayank-0103/";
+    }
+})
